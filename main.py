bd9df491b64e327947f4200bacec11f1e5bf28bd
AbdelrahmanHassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2023-07-13 00:50:47 +0300

    modifying design and adding matplotlib figure

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
-from PySide2.QtWidgets import QApplication, QMainWindow
-from PySide2.QtCore import QFile
-from ui_mainwindow import Ui_MainWindow
+from PySide2.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PySide2.QtGui import QDoubleValidator
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+from main_window_ui import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
@@ -10,11 +12,32 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        # Adding the matplotlib figure to the application
+        self.function_plotter_figure = Figure()
+        self.function_plotter_figure_canvas = FigureCanvas(
+            self.function_plotter_figure)
+        self.ui.Plotting_Layout.addWidget(self.function_plotter_figure_canvas)
+
+        # handling max and min value inputs to accept just numbers
+        validator = QDoubleValidator()
+        self.ui.min_value_input.setValidator(validator)
+        self.ui.max_value_input.setValidator(validator)
+
+    def raise_error(self, error_message):
+        ###
+        # function used to raise a given error message
+        ###
+        QMessageBox.about(self, "Error", error_message)
+
+    def raise_success(self, success_message):
+        ###
+        # function used to raise a given success message
+        ###
+        QMessageBox.about(self, "Success", success_message)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
     window = MainWindow()
     window.show()
-
     sys.exit(app.exec_())
--- main_window_ui.py
+++ main_window_ui.py
@@ -17,22 +17,153 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(695, 572)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.pushButton = QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(570, 60, 75, 23))
-        self.label = QLabel(self.centralwidget)
+        self.horizontalLayout_4 = QHBoxLayout(self.centralwidget)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.splitter = QSplitter(self.centralwidget)
+        self.splitter.setObjectName(u"splitter")
+        self.splitter.setOrientation(Qt.Vertical)
+        self.layoutWidget = QWidget(self.splitter)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.verticalLayout_6 = QVBoxLayout(self.layoutWidget)
+        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.verticalLayout_6.setContentsMargins(0, 0, 0, 0)
+        self.label_4 = QLabel(self.layoutWidget)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setMaximumSize(QSize(16777215, 58))
+        font = QFont()
+        font.setFamily(u"Umpush")
+        font.setPointSize(22)
+        self.label_4.setFont(font)
+        self.label_4.setStyleSheet(u"background-color:rgb(150,150,150);\n"
+"color:white")
+        self.label_4.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_6.addWidget(self.label_4)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.verticalLayout_4 = QVBoxLayout()
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.label = QLabel(self.layoutWidget)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(210, 40, 47, 13))
-        self.label_2 = QLabel(self.centralwidget)
+        self.label.setMaximumSize(QSize(16777215, 27))
+        font1 = QFont()
+        font1.setFamily(u"Ubuntu Condensed")
+        font1.setPointSize(16)
+        self.label.setFont(font1)
+        self.label.setStyleSheet(u"background-color:rgb(200,200,200)")
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_4.addWidget(self.label)
+
+        self.function_input = QLineEdit(self.layoutWidget)
+        self.function_input.setObjectName(u"function_input")
+        self.function_input.setMaximumSize(QSize(16777215, 27))
+        font2 = QFont()
+        font2.setPointSize(12)
+        self.function_input.setFont(font2)
+
+        self.verticalLayout_4.addWidget(self.function_input)
+
+
+        self.horizontalLayout_3.addLayout(self.verticalLayout_4)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.label_2 = QLabel(self.layoutWidget)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(210, 90, 47, 13))
+        self.label_2.setMaximumSize(QSize(100, 27))
+        font3 = QFont()
+        font3.setFamily(u"Ubuntu Condensed")
+        font3.setPointSize(14)
+        self.label_2.setFont(font3)
+        self.label_2.setStyleSheet(u"background-color:rgb(200,200,200)")
+        self.label_2.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_2.addWidget(self.label_2)
+
+        self.min_value_input = QLineEdit(self.layoutWidget)
+        self.min_value_input.setObjectName(u"min_value_input")
+        self.min_value_input.setMaximumSize(QSize(100, 27))
+        self.min_value_input.setFont(font2)
+
+        self.verticalLayout_2.addWidget(self.min_value_input)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout_2)
+
+        self.verticalLayout_5 = QVBoxLayout()
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.label_3 = QLabel(self.layoutWidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setMaximumSize(QSize(100, 27))
+        self.label_3.setFont(font3)
+        self.label_3.setStyleSheet(u"background-color:rgb(200,200,200)")
+        self.label_3.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_5.addWidget(self.label_3)
+
+        self.max_value_input = QLineEdit(self.layoutWidget)
+        self.max_value_input.setObjectName(u"max_value_input")
+        self.max_value_input.setMaximumSize(QSize(100, 27))
+        self.max_value_input.setFont(font2)
+
+        self.verticalLayout_5.addWidget(self.max_value_input)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout_5)
+
+
+        self.horizontalLayout_2.addLayout(self.horizontalLayout)
+
+        self.verticalLayout_7 = QVBoxLayout()
+        self.verticalLayout_7.setObjectName(u"verticalLayout_7")
+        self.pushButton = QPushButton(self.layoutWidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setMinimumSize(QSize(0, 28))
+        self.pushButton.setFont(font1)
+
+        self.verticalLayout_7.addWidget(self.pushButton)
+
+        self.pushButton_2 = QPushButton(self.layoutWidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setMinimumSize(QSize(0, 28))
+        font4 = QFont()
+        font4.setPointSize(13)
+        self.pushButton_2.setFont(font4)
+
+        self.verticalLayout_7.addWidget(self.pushButton_2)
+
+
+        self.horizontalLayout_2.addLayout(self.verticalLayout_7)
+
+
+        self.horizontalLayout_3.addLayout(self.horizontalLayout_2)
+
+
+        self.verticalLayout_6.addLayout(self.horizontalLayout_3)
+
+        self.splitter.addWidget(self.layoutWidget)
+        self.verticalLayoutWidget = QWidget(self.splitter)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.Plotting_Layout = QVBoxLayout(self.verticalLayoutWidget)
+        self.Plotting_Layout.setObjectName(u"Plotting_Layout")
+        self.Plotting_Layout.setContentsMargins(0, 0, 0, 0)
+        self.splitter.addWidget(self.verticalLayoutWidget)
+
+        self.horizontalLayout_4.addWidget(self.splitter)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 21))
+        self.menubar.setGeometry(QRect(0, 0, 695, 22))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -44,9 +175,12 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Function Plotter", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Function Plotter", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Function of x:", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Min Value:", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Max Value:", None))
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Plot", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"Input", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Input", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Save Figure", None))
     # retranslateUi
 
