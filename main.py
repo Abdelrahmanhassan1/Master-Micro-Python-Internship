58c4ebf8dbbae6df68731b262ace6661d33ce49a
AbdelrahmanHassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2023-07-13 07:16:25 +0300

    Adding save figure and user input functions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PySide2.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PySide2.QtWidgets import QApplication, QMainWindow, QMessageBox, QInputDialog
 from PySide2.QtGui import QDoubleValidator
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -12,6 +12,12 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        # global variables
+        self.number_of_points = 100
+
+        # Connecting the buttons to their functions
+        self.ui.pushButton_2.clicked.connect(self.save_figure_from_gui)
+
         # Adding the matplotlib figure to the application
         self.function_plotter_figure = Figure()
         self.function_plotter_figure_canvas = FigureCanvas(
@@ -22,6 +28,39 @@ class MainWindow(QMainWindow):
         validator = QDoubleValidator()
         self.ui.min_value_input.setValidator(validator)
         self.ui.max_value_input.setValidator(validator)
+
+    def check_entered_input_values(self, function, min, max):
+        ###
+        # function used to check the validity of the entered values
+        ###
+        try:
+            if function == "" or min == "" or max == "":
+                self.raise_error(
+                    "Function, min, and max value must be entered.")
+                return False
+
+            if float(max) <= float(min):
+                self.raise_error(
+                    "Maximum value must be greater than Minimum value.")
+                return False
+
+            return True
+        except Exception as e:
+            print(e)
+
+    def save_figure_from_gui(self):
+        ###
+        # function used to save the plotted equation figure from the gui
+        ###
+        figname, ok = QInputDialog.getText(
+            self, 'input dialog', 'Name of Figure:')
+        if ok:
+            try:
+                self.function_plotter_figure.savefig(
+                    f"figures_plotted/{figname}.png")
+                self.raise_success("Figure Saved Successfully")
+            except Exception as e:
+                self.raise_error("Error Figure Not Saved")
 
     def raise_error(self, error_message):
         ###
