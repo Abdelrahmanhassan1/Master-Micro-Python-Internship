72dca3eb7a90e308c747cde73f9e1cc018494388
AbdelrahmanHassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2023-07-17 16:09:23 +0300

    Adding plot function from user input equation

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from PySide2.QtWidgets import QApplication, QMainWindow, QMessageBox, QInputDial
 from PySide2.QtGui import QDoubleValidator
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+import sympy
+import numpy as np
 from main_window_ui import Ui_MainWindow
 
 
@@ -14,9 +16,12 @@ class MainWindow(QMainWindow):
 
         # global variables
         self.number_of_points = 100
-
-        # Connecting the buttons to their functions
-        self.ui.pushButton_2.clicked.connect(self.save_figure_from_gui)
+        self.max_range_value = 0
+        self.min_range_value = 0
+        self.valid_chars = ["x", "1", "2", "3", "4", "5", "6",
+                            "7", "8", "9", "0", "*", "/", "-", "^", "+", ".", " "]
+        self.operators = ["*", "/", "^"]
+        self.x_symbol = sympy.symbols("x")
 
         # Adding the matplotlib figure to the application
         self.function_plotter_figure = Figure()
@@ -28,6 +33,10 @@ class MainWindow(QMainWindow):
         validator = QDoubleValidator()
         self.ui.min_value_input.setValidator(validator)
         self.ui.max_value_input.setValidator(validator)
+
+        # connecting the buttons to their functions
+        self.ui.pushButton.clicked.connect(self.get_user_function)
+        self.ui.pushButton_2.clicked.connect(self.save_figure_from_gui)
 
     def check_entered_input_values(self, function, min, max):
         ###
@@ -45,6 +54,149 @@ class MainWindow(QMainWindow):
                 return False
 
             return True
+        except Exception as e:
+            print(e)
+
+    def get_user_function(self):
+        try:
+            self.user_function = self.ui.function_input.text().strip()
+            self.max_range_value = self.ui.max_value_input.text()
+            self.min_range_value = self.ui.min_value_input.text()
+
+            # check for input validation
+            if self.check_entered_input_values(
+                    self.user_function, self.min_range_value, self.max_range_value):
+
+                self.min_range_value = float(self.min_range_value)
+                self.max_range_value = float(self.max_range_value)
+
+                equation_expressions = self.calculate_function_expression(
+                    self.user_function)
+
+                if not equation_expressions:
+                    return
+
+                range_values = np.linspace(
+                    self.min_range_value, self.max_range_value, self.number_of_points)
+
+                full_equation = 0
+                x_axis = []
+                y_axis = []
+
+                for expression in equation_expressions:
+                    full_equation += expression
+
+                # check if the expression is a single number
+                if not (isinstance(full_equation, float)):
+                    for i in range_values:
+                        new_y = full_equation.subs(self.x_symbol, i)
+                        if new_y == sympy.zoo:
+                            self.raise_error(
+                                f"The value x={i} after substitution cause infinity!!")
+                            x_axis = []
+                            y_axis = []
+                            break
+                        x_axis.append(i)
+                        y_axis.append(new_y)
+
+                else:
+                    x_axis = range_values
+                    y_axis = np.repeat(full_equation, len(range_values))
+
+                if (len(x_axis) > 0 or len(y_axis) > 0):
+                    figure_axes = self.function_plotter_figure.gca()
+                    figure_axes.cla()
+                    figure_axes.grid(True)
+                    figure_axes.set_facecolor((1, 1, 1))
+                    figure_axes.set_title("Equation: " + str(full_equation))
+                    figure_axes.set_xlabel("x axis")
+                    figure_axes.set_ylabel("y axis")
+                    figure_axes.plot(x_axis, y_axis)
+
+                self.function_plotter_figure_canvas.draw()
+                self.function_plotter_figure_canvas.flush_events()
+        except Exception as e:
+            print(e)
+
+    def calculate_function_expression(self, function):
+        try:
+            added_expressions = []
+            new_expression = 1
+
+            index = 0
+            while True:
+                if index == len(function):
+                    added_expressions.append(new_expression)
+                    break
+
+                char = function[index]
+                if char == "x":
+                    new_expression *= self.x_symbol
+                # if having a power character
+                elif char == "^":
+                    index += 1
+                    power = np.double(function[index])
+
+                    while (power > 1):
+                        if function[index-2] == "x":
+                            new_expression *= self.x_symbol
+                        elif function[index-2].isdigit():
+                            new_expression *= np.double(function[index-2])
+                        power -= 1
+
+                elif char == "*":
+                    index += 1
+                    if (function[index] == "x"):
+                        new_expression *= self.x_symbol
+                    elif (function[index].isdigit()):
+                        new_expression *= np.double(function[index])
+
+                elif char == "/":
+                    index += 1
+                    if (function[index] == "x"):
+                        new_expression /= self.x_symbol
+                    elif (function[index].isdigit()):
+                        new_expression /= np.double(function[index])
+
+                elif char.isdigit():
+                    first_index = index
+                    while True:
+                        index += 1
+                        if index == len(function):
+                            break
+                        next_char = function[index]
+                        if next_char.isdigit() or next_char == ".":
+                            continue
+                        else:
+                            break
+
+                    last_index = index
+                    index -= 1
+                    if first_index == last_index:
+                        new_expression *= np.double(function[first_index])
+                    else:
+                        new_expression *= np.double(
+                            function[first_index:last_index])
+
+                elif char == "+":
+                    if index != 0:
+                        added_expressions.append(new_expression)
+                    new_expression = 1
+
+                elif char == "-":
+                    if index != 0:
+                        added_expressions.append(new_expression)
+                    new_expression = -1
+
+                elif char == ".":
+                    self.raise_error(
+                        "Floating Numbers must start with Digit. (i.e 0.21)")
+                    added_expressions = []
+                    new_expression = 1
+                    return False
+
+                index += 1
+            return added_expressions
         except Exception as e:
             print(e)
 
